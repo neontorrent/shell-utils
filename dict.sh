8b51a5e83839fa043e0d10d84cc2dee56ff607ba
neontorrent <1185132+neontorrent@users.noreply.github.com>
2025-06-27 17:21:43 -0700

    Add dict example

--- dict.sh
+++ dict.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 dict_set_value() {
-    dict=$1
-    key=$2
-    value=$3
+    dict="$1"
+    key="$2"
+    value="$3"
     internal_prefix="_dict_${dict}_"
     eval "$internal_prefix$key=\"\$value\""
 }
 
 dict_get_value() {
-    dict=$1
-    key=$2
+    dict="$1"
+    key="$2"
     internal_prefix="_dict_${dict}_"
     eval echo "\$$internal_prefix$key"
 }
 
 dict_for() {
-    dict=$1
+    dict="$1"
     func="$2"
     internal_prefix="_dict_${dict}_"
 
@@ -26,3 +26,21 @@ dict_for() {
         "$func" "$key" "$value"
     done
 }
+
+# Example
+foo="foo"
+dict_set_value "$foo" "a" 1
+dict_set_value "$foo" "b" 2
+
+dict_get_value "$foo" "a" 
+# print 1
+
+process_foo() {
+  key="$1"
+  value="$2"
+  echo "There is a key $key and value $value"
+}
+dict_for "$foo" process_foo
+# print:
+# There is a key a and value 1
+# There is a key b and value 2
